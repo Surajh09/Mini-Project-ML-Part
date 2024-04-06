b928f3165369f45ab0605e33cef504f2601ac6ca
Surajh09 <98706142+Surajh09@users.noreply.github.com>
2024-04-06 11:01:01 +0000

    master

--- Attendence_System/settings.py
+++ Attendence_System/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,10 +79,12 @@ WSGI_APPLICATION = 'Attendence_System.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASE['default'] = dj_database_url.parse("postgres://mini_project_xx2b_user:kjkTjpf5ek1NQjyhWT5vY5thuletxDsv@dpg-co7oi7sf7o1s738k28m0-a.oregon-postgres.render.com/mini_project_xx2b")
+
 
 
 # Password validation
