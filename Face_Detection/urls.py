7df0c522999e703dee0d4a9dbc13e7c90e379407
Surajh09 <98706142+Surajh09@users.noreply.github.com>
2024-04-22 12:17:10 +0530

    Update urls.py

--- Face_Detection/urls.py
+++ Face_Detection/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('',home,name = "home"),
     path('register/',register,name = 'register'),
@@ -9,4 +12,4 @@ urlpatterns = [
     path('team/',team,name = 'team'),
     path('greeting/<face_id>/',Greeting,name='greeting'),
     path('staff/',searchAttendence,name='staff'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
