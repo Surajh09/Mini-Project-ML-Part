6b28260fe9dacdc543aa927ba2098d3cd9251c15
Surajh09 <98706142+Surajh09@users.noreply.github.com>
2024-04-22 12:19:11 +0530

    Update settings.py

--- FaceDetection/settings.py
+++ FaceDetection/settings.py
@@ -101,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -121,9 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles') 
 
 MEDIA_URL = '/media/'
